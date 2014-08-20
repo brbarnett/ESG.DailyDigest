22a45a3ad4ac14d866385578a4ce9256bbc021de
brbarnett <barnett@echosolutionsgroup.com>
2014-08-20 12:50:27 -0500

    Added Mapper class for creating EmailItemRow models

---
+++ ESG.DailyDigest.Infrastructure/Mapper.cs
@@ -0,0 +1,34 @@
+﻿using ESG.DailyDigest.Domain.Models;
+
+namespace ESG.DailyDigest.Infrastructure
+{
+    public class Mapper
+    {
+        public static EmailItemRow Create(SportingEvent item)
+        {
+            EmailItemRow row = new EmailItemRow();
+            row.Icon = item.LogoImageUrl;
+            row.Text = item.IsValid ? string.Format("Cubs game today at {0} {1} {2}",
+                item.DateTime.ToString("hh:mm tt"),
+                item.IsHomeGame ? "vs" : "@",
+                item.Opponent) : "No data";
+            return row;
+        }
+
+        public static EmailItemRow Create(WeatherForecast item)
+        {
+            EmailItemRow row = new EmailItemRow();
+            row.Icon = item.ForecastIcon;
+            row.Text = item.IsValid ? item.ForecastText : "No data";
+            return row;
+        }
+
+        public static EmailItemRow Create(TrainStatus item)
+        {
+            EmailItemRow row = new EmailItemRow();
+            row.Icon = item.LogoUrl;
+            row.Text = item.IsValid ? item.Table : "No data";
+            return row;
+        }
+    }
+}
